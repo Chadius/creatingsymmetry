7f25d5ae5a00dc0a63fbce7ee1b8484a84606fe2
Chad Serrant <therealchadius@gmail.com>
2021-04-06 19:47:57 -0400

    Add Numerically Close checker [#177636864]

---
+++ entities/utility/testUtility.go
@@ -0,0 +1,41 @@
+package utility
+
+import (
+	"fmt"
+	. "gopkg.in/check.v1"
+	"math"
+	"reflect"
+)
+
+// NumericallyCloseEnough asserts the two given numbers are within tolerance.
+type NumericallyCloseEnough struct {}
+
+// Info returns information for the NumericallyCloseEnough
+func (checker NumericallyCloseEnough) Info() *CheckerInfo {
+	return &CheckerInfo{Name: "NumericallyCloseEnough", Params: []string{"obtained", "expected", "tolerance"}}
+}
+
+// Check to see if the obtained and expected are within tolerance.
+func (checker NumericallyCloseEnough) Check(params []interface{}, names []string) (result bool, error string) {
+	interfaceObtained := params[0]
+	interfaceExpected := params[1]
+	interfaceTolerance := params[2]
+
+	var floatType = reflect.TypeOf(float64(0))
+
+	v := reflect.ValueOf(interfaceObtained)
+	v = reflect.Indirect(v)
+	obtained := v.Convert(floatType).Float()
+	v = reflect.ValueOf(interfaceExpected)
+	v = reflect.Indirect(v)
+	expected := v.Convert(floatType).Float()
+	v = reflect.ValueOf(interfaceTolerance)
+	v = reflect.Indirect(v)
+	tolerance := v.Convert(floatType).Float()
+
+	if math.Abs(obtained - expected) < tolerance {
+		return true, ""
+	}
+
+	return false, fmt.Sprintf("%f and %f are outside of tolerance %f", obtained, expected, tolerance)
+}
---
+++ entities/utility/testUtility_test.go
@@ -0,0 +1,43 @@
+package utility_test
+
+import (
+	. "gopkg.in/check.v1"
+	"testing"
+	"wallpaper/entities/utility"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type CloseEnoughTests struct {
+}
+
+var _ = Suite(&CloseEnoughTests{})
+
+func (suite *CloseEnoughTests) SetUpTest(checker *C) {}
+
+func (suite *CloseEnoughTests) TestExactNumbersAreCloseEnough(checker *C) {
+	checker.Assert(0.0, utility.NumericallyCloseEnough{}, 0.0, 0.1)
+}
+
+func (suite *CloseEnoughTests) TestCloseNumbersAreCloseEnough(checker *C) {
+	checker.Assert(0.0, utility.NumericallyCloseEnough{}, 0.1, 0.2)
+}
+
+func (suite *CloseEnoughTests) TestVeryDifferentNumbersAreNotCloseEnough(checker *C) {
+	numericallyCloseEnoughCheck := utility.NumericallyCloseEnough{}
+
+	matcherArguments := []float64{0.0, 10.0, 0.1}
+	interfaceOfFloats := make([]interface{}, len(matcherArguments))
+	for index, flo := range matcherArguments {
+		interfaceOfFloats[index] = flo
+	}
+
+	result, err := numericallyCloseEnoughCheck.Check( interfaceOfFloats, []string{"obtained", "expected", "tolerance"})
+
+	checker.Assert(result, Equals, false)
+	checker.Assert(err, Equals, "0.000000 and 10.000000 are outside of tolerance 0.100000")
+}
+
+func (suite *CloseEnoughTests) TestDifferentNumbersCanBeWithinTolerance(checker *C) {
+	checker.Assert(0.0, utility.NumericallyCloseEnough{}, 10.0, 20.0)
+}
